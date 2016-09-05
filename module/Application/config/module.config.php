c25612cade8f705c250024d6c5970faff0904727
Quentin Bonaventure <quentin@bonaventure.xyz>
2016-09-05 16:32:37 +0200

    Update module.config.php

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -121,14 +121,4 @@ return array(
             ),
         ),
     ),
-	'mail' => array(
-		'host'              => 'imap.bonaventure.xyz',
-		'port'				=> 587,
-		'connection_class'  => 'plain',
-		'connection_config' => array(
-				'username' => 'quentin@bonaventure.xyz',
-				'password' => 'bNMpz2kjEf8nlH71sHESXEWf2sSrQp',
-				'ssl'	=> 'tls',
-		),
-	),
 );
