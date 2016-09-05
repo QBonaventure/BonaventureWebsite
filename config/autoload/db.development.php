4aac7d28662e9a55401300c9af4d93afd10760bb
Quentin Bonaventure <quentin@bonaventure.xyz>
2016-09-05 16:21:53 +0200

    Delete db.development.php

--- config/autoload/db.development.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'db' => array(
-		'driver'   => 'Pdo_Pgsql',
-		'dsn'      => 'pgsql:dbname=bonaventure;host=107.6.124.52;port=2709',
-		'username' => 'raziel',
-		'password' => 'Mc`XuMeqOcK"+]Z&GLdMhyz=ZIiI)7',
-	),
-);
